357545e2e6c9fadb32b91242d7f5bdc836ebc6b6
Alexander Stryabkov <striabkov.av@reliab.tech>
2020-03-26 13:30:13 +0300

    add port param

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const DEFAULT_PORT = 3000
+
+const argv = process.argv.slice(2)
+const argPort = argv.find(arg => arg.includes('port'))
+const port = argPort ? Number(argPort.slice(argPort.indexOf('=') + 1)) : DEFAULT_PORT
+
 app.use(express.static('src'));
 
 app.get('/', function(req, res) {
   res.redirect('index.html');
 });
 
-app.listen(8080);
+app.listen(port);
 
-console.log('hello there');
+console.log('Server started at http://localhost:' + String(port));
